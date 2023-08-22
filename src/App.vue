19f739acf7d811b5a9151627f0f69a0a4b6fa0e9
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-08-22 13:17:50 +0600

    Top bar Update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <TopBar />
+    <!-- Rest of your content goes here -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TopBar,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,135 @@
+<template>
+    <div>
+      <!-- Emergency Top Bar -->
+      <div class="emergency-top-bar">
+        <p>Emergency Topic Bar</p>
+      </div>
+  
+      <!-- E-Commerce Top Bar -->
+      <div class="ecommerce-top-bar">
+        <div class="logo">
+          <img src="/path-to-your-logo.png" alt="Logo" />
+        </div>
+        <div class="buttons">
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </div>
+      </div>
+      <!-- Video Panel -->
+    <div class="video-panel">
+      <video controls>
+        <source src="/path-to-your-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+
+       <!-- Picture Panel -->
+    <div class="picture-panel">
+      <img src="/path-to-your-picture.png" alt="Picture" />
+    </div>
+
+    <!-- Second Picture Panel (side by side) -->
+    <div class="second-picture-panel">
+      <div class="picture">
+        <img src="/path-to-your-picture1.png" alt="Picture 1" />
+      </div>
+      <div class="picture">
+        <img src="/path-to-your-picture2.png" alt="Picture 2" />
+      </div>
+    </div>
+
+
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="contact-info">
+        <p>Contact us: email@example.com</p>
+        <p>Phone: +123456789</p>
+      </div>
+      <div class="copyright">
+        <p>&copy; 2023 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+
+    </div>
+  </template>
+  
+  <script setup>
+  // You can import any required modules here
+  </script>
+  
+  <style scoped>
+  /* Style your top bars here */
+  .emergency-top-bar {
+    background-color: red;
+    color: white;
+    text-align: center;
+    padding: 10px;
+  }
+  
+  .ecommerce-top-bar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #f0f0f0;
+    padding: 10px;
+  }
+  
+  .logo img {
+    max-height: 50px;
+  }
+  
+  .buttons button {
+    margin-left: 10px;
+  }
+
+  .video-panel {
+  text-align: center;
+  padding: 20px;
+}
+
+.video-panel video {
+  max-width: 100%;
+  height: auto;
+}
+
+  .picture-panel {
+  text-align: center;
+  padding: 20px;
+}
+
+.picture-panel img {
+  max-width: 100%;
+  height: auto;
+}
+
+.second-picture-panel {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 20px;
+}
+
+.second-picture-panel .picture {
+  flex-basis: calc(50% - 10px);
+  margin: 5px;
+}
+
+.second-picture-panel img {
+  max-width: 100%;
+  height: auto;
+}
+
+.footer {
+  background-color: #f0f0f0;
+  padding: 20px;
+  text-align: center;
+}
+
+.contact-info p,
+.copyright p {
+  margin: 5px 0;
+}
+  
+  </style>
+  
