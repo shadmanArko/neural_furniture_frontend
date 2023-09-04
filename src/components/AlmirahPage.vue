3940f325506fe9d9ccee1f1bbdb9e177a5f6f2f8
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2023-09-04 23:00:55 +0600

    Update AlmirahPage.vue

--- src/components/AlmirahPage.vue
+++ src/components/AlmirahPage.vue
@@ -1,8 +1,21 @@
-<template>
-    <div>
-      <h1>Almirah Collection</h1>
-      <!-- Display your almirah collection content here -->
-    </div>
+ <template>
+   
+   <div>
+     <h1>Almirah Collection</h1>
+     <!-- Display your almirah collection content here -->
+   </div>
+   
+   <div class="product-page">
+     <div class="product-container">
+       <img src="../assets/BedroomSet.png" alt="Picture" />
+       <!-- Product details go here -->
+     </div>
+     <div class="product-description">
+       <!-- Product description goes here -->
+       <p>Description of Sofa</p>
+     </div>
+   </div>
+   
   </template>
   
   <script>
@@ -12,6 +25,38 @@
   </script>
   
   <style scoped>
-  /* Add styles specific to the Bed page */
+    .product-page {
+      display: flex;
+    }
+  
+    .product-container {
+      flex: 1;
+      width: 50%;
+    }
+    .product-container img {
+      max-width: 100%; /* Ensure the image doesn't exceed the container width */
+      height: auto; /* Maintain the aspect ratio */
+      display: block; /* Remove any default inline spacing */
+      margin: 0 auto; /* Center the image horizontally within the container */
+    }
+  
+    .product-description {
+      flex: 1;
+    }
+  
+    /* For mobile view */
+    @media (max-width: 768px) {
+      .product-page {
+        flex-direction: column;
+      }
+  
+      .product-container {
+        width: 100%;
+      }
+  
+      .product-description {
+        width: 100%;
+      }
+    }
   </style>
   
